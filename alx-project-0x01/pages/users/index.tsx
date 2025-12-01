8c077d615dbdcb909e737308594a373ec4b9d889
Emmanuel Ohwoka <emmyprime2025@gmail.com>
2025-12-02 00:34:39 +0100

    add a postcard

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,13 @@
+import Header from '@/components/layout/Header';
+
 const Users: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-7xl font-thin">Users Page</h1>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-grow p-4">
+        <h1 className="text-2xl font-semibold mb-4">User List</h1>
+        <p>This is the users page. User details will be displayed here.</p>
+      </main>
     </div>
   )
 }
